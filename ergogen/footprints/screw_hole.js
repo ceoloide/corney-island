5cfa2b8ef01b578aa9eb58926ec7f0f698a1e5fc
Marco Massarelli <marco.massarelli@gmail.com>
2023-08-14 10:09:17 -0400

    Update screw hole definition

--- ergogen/footprints/screw_hole.js
+++ ergogen/footprints/screw_hole.js
@@ -5,10 +5,9 @@ module.exports = {
       drill: '2.2'
   },
   body: p => `
-  (module Screw_Hole (layer F.Cu) (tedit 5F1B9159) (tstamp 61905781)
+  (module "Screw_Hole" (layer F.Cu) (tedit 5F1B9159)
       ${p.at /* parametric position */}
       (fp_text reference "${p.ref}" (at 0 2.55) (layer F.SilkS) ${p.ref_hide} (effects (font (size 1 1) (thickness 0.15))))
-      (fp_text value "Screw hole (NPTH)" (at 0 -0.5) (layer F.Fab) (effects (font (size 1 1) (thickness 0.15))))
       (pad "" np_thru_hole circle (at 0 0) (size ${p.size} ${p.size}) (drill ${p.drill}) (layers *.Cu *.Mask))
   )
   `
