88728e127d5850fa7302de273ab75fbad3efad73
Marco Massarelli <marco.massarelli@gmail.com>
2023-11-26 18:55:38 +0000

    Update silkscreen

--- ergogen/footprints/jst_battery_connector.js
+++ ergogen/footprints/jst_battery_connector.js
@@ -60,7 +60,7 @@ module.exports = {
                     (effects (font (size 1 1) (thickness 0.15)))
                 )
             `
-            const front_fabrication = `
+        const front_fabrication = `
                 (fp_line (start -2.95 -1.35) (end -2.95 6.25) (width 0.1) (layer "F.Fab"))
                 (fp_line (start -2.95 6.25) (end 2.95 6.25) (width 0.1) (layer "F.Fab"))
                 (fp_line (start -2.25 -1.35) (end -2.95 -1.35) (width 0.1) (layer "F.Fab"))
@@ -69,17 +69,17 @@ module.exports = {
                 (fp_line (start 2.25 0.25) (end -2.25 0.25) (width 0.1) (layer "F.Fab"))
                 (fp_line (start 2.95 -1.35) (end 2.25 -1.35) (width 0.1) (layer "F.Fab"))
                 (fp_line (start 2.95 6.25) (end 2.95 -1.35) (width 0.1) (layer "F.Fab"))
-                `
-            const front_courtyard = `
+        `
+        const front_courtyard = `
                 (fp_line (start -3.45 -1.85) (end -3.45 8.5) (width 0.05) (layer "F.CrtYd"))
                 (fp_line (start -3.45 8.5) (end 3.45 8.5) (width 0.05) (layer "F.CrtYd"))
                 (fp_line (start 3.45 -1.85) (end -3.45 -1.85) (width 0.05) (layer "F.CrtYd"))
                 (fp_line (start 3.45 8.5) (end 3.45 -1.85) (width 0.05) (layer "F.CrtYd"))
-            `
-            const front_silkscreen = `
+        `
+        const front_silkscreen = `
+                (fp_line (start 1.5 4.375) (end 0.5 4.375) (width 0.1) (layer "F.SilkS"))
                 (fp_line (start -1.5 4.375) (end -0.5 4.375) (width 0.1) (layer "F.SilkS"))
-                (fp_line (start -1 3.875) (end -1.5 4.375) (width 0.1) (layer "F.SilkS"))
-                (fp_line (start -0.5 4.375) (end -1 3.875) (width 0.1) (layer "F.SilkS"))
+                (fp_line (start -1 3.875) (end -1 4.875) (width 0.1) (layer "F.SilkS"))
                 (fp_line (start -2.06 -1.46) (end -3.06 -1.46) (width 0.12) (layer "F.SilkS"))
                 (fp_line (start -3.06 -1.46) (end -3.06 -0.46) (width 0.12) (layer "F.SilkS"))
                 (fp_line (start 2.14 -1.46) (end 3.06 -1.46) (width 0.12) (layer "F.SilkS"))
@@ -88,11 +88,8 @@ module.exports = {
                 (fp_line (start -3.06 6.36) (end -3.06 5.36) (width 0.12) (layer "F.SilkS"))
                 (fp_line (start 2.14 6.36) (end 3.06 6.36) (width 0.12) (layer "F.SilkS"))
                 (fp_line (start 3.06 6.36) (end 3.06 5.36) (width 0.12) (layer "F.SilkS"))
-            `
-            const back_fabrication = `
-                (fp_line (start 1.5 4.375) (end 0.5 4.375) (width 0.1) (layer "B.SilkS"))
-                (fp_line (start 1 3.875) (end 1.5 4.375) (width 0.1) (layer "B.SilkS"))
-                (fp_line (start 0.5 4.375) (end 1 3.875) (width 0.1) (layer "B.SilkS"))
+        `
+        const back_fabrication = `
                 (fp_line (start -2.95 -1.35) (end -2.25 -1.35) (width 0.1) (layer "B.Fab"))
                 (fp_line (start -2.95 6.25) (end -2.95 -1.35) (width 0.1) (layer "B.Fab"))
                 (fp_line (start -2.25 -1.35) (end -2.25 0.25) (width 0.1) (layer "B.Fab"))
@@ -101,151 +98,154 @@ module.exports = {
                 (fp_line (start 2.25 0.25) (end 2.25 -1.35) (width 0.1) (layer "B.Fab"))
                 (fp_line (start 2.95 -1.35) (end 2.95 6.25) (width 0.1) (layer "B.Fab"))
                 (fp_line (start 2.95 6.25) (end -2.95 6.25) (width 0.1) (layer "B.Fab"))
-                `
-            const back_courtyard = `
-            (fp_line (start -3.45 -1.85) (end -3.45 8.5) (width 0.05) (layer "B.CrtYd"))
-            (fp_line (start -3.45 8.5) (end 3.45 8.5) (width 0.05) (layer "B.CrtYd"))
-            (fp_line (start 3.45 -1.85) (end -3.45 -1.85) (width 0.05) (layer "B.CrtYd"))
-            (fp_line (start 3.45 8.5) (end 3.45 -1.85) (width 0.05) (layer "B.CrtYd"))
-            `
-            const back_silkscreen = `
-            (fp_line (start -2.06 -1.46) (end -3.06 -1.46) (width 0.12) (layer "B.SilkS"))
-            (fp_line (start -3.06 -1.46) (end -3.06 -0.46) (width 0.12) (layer "B.SilkS"))
-            (fp_line (start 2.14 -1.46) (end 3.06 -1.46) (width 0.12) (layer "B.SilkS"))
-            (fp_line (start 3.06 -1.46) (end 3.06 -0.46) (width 0.12) (layer "B.SilkS"))
-            (fp_line (start -2.14 6.36) (end -3.06 6.36) (width 0.12) (layer "B.SilkS"))
-            (fp_line (start -3.06 6.36) (end -3.06 5.36) (width 0.12) (layer "B.SilkS"))
-            (fp_line (start 2.14 6.36) (end 3.06 6.36) (width 0.12) (layer "B.SilkS"))
-            (fp_line (start 3.06 6.36) (end 3.06 5.36) (width 0.12) (layer "B.SilkS"))
-            `
+        `
+        const back_courtyard = `
+                (fp_line (start -3.45 -1.85) (end -3.45 8.5) (width 0.05) (layer "B.CrtYd"))
+                (fp_line (start -3.45 8.5) (end 3.45 8.5) (width 0.05) (layer "B.CrtYd"))
+                (fp_line (start 3.45 -1.85) (end -3.45 -1.85) (width 0.05) (layer "B.CrtYd"))
+                (fp_line (start 3.45 8.5) (end 3.45 -1.85) (width 0.05) (layer "B.CrtYd"))
+        `
+        const back_silkscreen = `
+                (fp_line (start -1.5 4.375) (end -0.5 4.375) (width 0.1) (layer "B.SilkS"))
+                (fp_line (start 1.5 4.375) (end 0.5 4.375) (width 0.1) (layer "B.SilkS"))
+                (fp_line (start 1 3.875) (end 1 4.875) (width 0.1) (layer "B.SilkS"))
+                (fp_line (start -2.06 -1.46) (end -3.06 -1.46) (width 0.12) (layer "B.SilkS"))
+                (fp_line (start -3.06 -1.46) (end -3.06 -0.46) (width 0.12) (layer "B.SilkS"))
+                (fp_line (start 2.14 -1.46) (end 3.06 -1.46) (width 0.12) (layer "B.SilkS"))
+                (fp_line (start 3.06 -1.46) (end 3.06 -0.46) (width 0.12) (layer "B.SilkS"))
+                (fp_line (start -2.14 6.36) (end -3.06 6.36) (width 0.12) (layer "B.SilkS"))
+                (fp_line (start -3.06 6.36) (end -3.06 5.36) (width 0.12) (layer "B.SilkS"))
+                (fp_line (start 2.14 6.36) (end 3.06 6.36) (width 0.12) (layer "B.SilkS"))
+                (fp_line (start 3.06 6.36) (end 3.06 5.36) (width 0.12) (layer "B.SilkS"))
+        `
         const front_pads = `
-            (pad 1 thru_hole roundrect (at -1 0 ${p.rot}) (size 1.2 1.75) (drill 0.75) (layers "*.Cu" "*.Mask") (roundrect_rratio 0.208333) ${p.BAT_P.str})
-            (pad 2 thru_hole oval (at 1 0 ${p.rot}) (size 1.2 1.75) (drill 0.75) (layers "*.Cu" "*.Mask") ${p.BAT_N.str})
+                (pad 1 thru_hole roundrect (at -1 0 ${p.rot}) (size 1.2 1.75) (drill 0.75) (layers "*.Cu" "*.Mask") (roundrect_rratio 0.208333) ${p.BAT_P.str})
+                (pad 2 thru_hole oval (at 1 0 ${p.rot}) (size 1.2 1.75) (drill 0.75) (layers "*.Cu" "*.Mask") ${p.BAT_N.str})
         `
         const back_pads = `
-            (pad 1 thru_hole roundrect (at 1 0 ${p.rot}) (size 1.2 1.75) (drill 0.75) (layers "*.Cu" "*.Mask") (roundrect_rratio 0.208333) ${p.BAT_P.str})
-            (pad 2 thru_hole oval (at -1 0 ${p.rot}) (size 1.2 1.75) (drill 0.75) (layers "*.Cu" "*.Mask") ${p.BAT_N.str})
+                (pad 1 thru_hole roundrect (at 1 0 ${p.rot}) (size 1.2 1.75) (drill 0.75) (layers "*.Cu" "*.Mask") (roundrect_rratio 0.208333) ${p.BAT_P.str})
+                (pad 2 thru_hole oval (at -1 0 ${p.rot}) (size 1.2 1.75) (drill 0.75) (layers "*.Cu" "*.Mask") ${p.BAT_N.str})
         `
         const reversible_pads = `
-            (pad 11 thru_hole oval (at -1 0 ${p.rot}) (size 1.2 1.75) (drill 0.75) (layers "*.Cu" "*.Mask") ${local_nets[0].str})
-            (pad 12 thru_hole oval (at 1 0 ${p.rot}) (size 1.2 1.75) (drill 0.75) (layers "*.Cu" "*.Mask") ${local_nets[1].str})
-            (pad 21 smd custom (at -1 1.8 ${180 + p.rot}) (size 0.1 0.1) (layers "F.Cu" "F.Mask")
-                (clearance 0.1) (zone_connect 0)
-                (options (clearance outline) (anchor rect))
-                (primitives
-                (gr_poly
-                    (pts
-                    (xy 0.6 0.4)
-                    (xy -0.6 0.4)
-                    (xy -0.6 0.2)
-                    (xy 0 -0.4)
-                    (xy 0.6 0.2)
-                    )   
-                    (width 0))
-                )
-                ${local_nets[0]})
-            (pad 31 smd custom (at -1 1.8 ${180 + p.rot}) (size 0.1 0.1) (layers "B.Cu" "B.Mask")
-                (clearance 0.1) (zone_connect 0)
-                (options (clearance outline) (anchor rect))
-                (primitives
-                (gr_poly
-                    (pts
-                    (xy 0.6 0.4)
-                    (xy -0.6 0.4)
-                    (xy -0.6 0.2)
-                    (xy 0 -0.4)
-                    (xy 0.6 0.2)
+                (pad 11 thru_hole oval (at -1 0 ${p.rot}) (size 1.2 1.75) (drill 0.75) (layers "*.Cu" "*.Mask") ${local_nets[0].str})
+                (pad 12 thru_hole oval (at 1 0 ${p.rot}) (size 1.2 1.75) (drill 0.75) (layers "*.Cu" "*.Mask") ${local_nets[1].str})
+                (pad 21 smd custom (at -1 1.8 ${180 + p.rot}) (size 0.1 0.1) (layers "F.Cu" "F.Mask")
+                    (clearance 0.1) (zone_connect 0)
+                    (options (clearance outline) (anchor rect))
+                    (primitives
+                    (gr_poly
+                        (pts
+                        (xy 0.6 0.4)
+                        (xy -0.6 0.4)
+                        (xy -0.6 0.2)
+                        (xy 0 -0.4)
+                        (xy 0.6 0.2)
+                        )   
+                        (width 0))
                     )
-                    (width 0))
-                )
-                ${local_nets[0]})
-            (pad 22 smd custom (at 1 1.8 ${180 + p.rot}) (size 0.1 0.1) (layers "F.Cu" "F.Mask")
-                (clearance 0.1) (zone_connect 0)
-                (options (clearance outline) (anchor rect))
-                (primitives
-                (gr_poly
-                    (pts
-                    (xy 0.6 0.4)
-                    (xy -0.6 0.4)
-                    (xy -0.6 0.2)
-                    (xy 0 -0.4)
-                    (xy 0.6 0.2)
+                    ${local_nets[0]})
+                (pad 31 smd custom (at -1 1.8 ${180 + p.rot}) (size 0.1 0.1) (layers "B.Cu" "B.Mask")
+                    (clearance 0.1) (zone_connect 0)
+                    (options (clearance outline) (anchor rect))
+                    (primitives
+                    (gr_poly
+                        (pts
+                        (xy 0.6 0.4)
+                        (xy -0.6 0.4)
+                        (xy -0.6 0.2)
+                        (xy 0 -0.4)
+                        (xy 0.6 0.2)
+                        )
+                        (width 0))
                     )
-                    (width 0))
-                )
-                ${local_nets[1]})
-            (pad 32 smd custom (at 1 1.8 ${180 + p.rot}) (size 0.1 0.1) (layers "B.Cu" "B.Mask")
-                (clearance 0.1) (zone_connect 0)
-                (options (clearance outline) (anchor rect))
-                (primitives
-                (gr_poly
-                    (pts
-                    (xy 0.6 0.4)
-                    (xy -0.6 0.4)
-                    (xy -0.6 0.2)
-                    (xy 0 -0.4)
-                    (xy 0.6 0.2)
+                    ${local_nets[0]})
+                (pad 22 smd custom (at 1 1.8 ${180 + p.rot}) (size 0.1 0.1) (layers "F.Cu" "F.Mask")
+                    (clearance 0.1) (zone_connect 0)
+                    (options (clearance outline) (anchor rect))
+                    (primitives
+                    (gr_poly
+                        (pts
+                        (xy 0.6 0.4)
+                        (xy -0.6 0.4)
+                        (xy -0.6 0.2)
+                        (xy 0 -0.4)
+                        (xy 0.6 0.2)
+                        )
+                        (width 0))
                     )
-                    (width 0))
-                )
-                ${local_nets[1]})
-            (pad 1 smd custom (at -1 2.816 ${180 + p.rot}) (size 1.2 0.5) (layers "F.Cu" "F.Mask") ${p.BAT_N.str}
-                (clearance 0.1) (zone_connect 0)
-                (options (clearance outline) (anchor rect))
-                (primitives
-                (gr_poly
-                    (pts
-                    (xy 0.6 0)
-                    (xy -0.6 0)
-                    (xy -0.6 1)
-                    (xy 0 0.4)
-                    (xy 0.6 1)
+                    ${local_nets[1]})
+                (pad 32 smd custom (at 1 1.8 ${180 + p.rot}) (size 0.1 0.1) (layers "B.Cu" "B.Mask")
+                    (clearance 0.1) (zone_connect 0)
+                    (options (clearance outline) (anchor rect))
+                    (primitives
+                    (gr_poly
+                        (pts
+                        (xy 0.6 0.4)
+                        (xy -0.6 0.4)
+                        (xy -0.6 0.2)
+                        (xy 0 -0.4)
+                        (xy 0.6 0.2)
+                        )
+                        (width 0))
                     )
-                    (width 0))
-                ) )
-            (pad 1 smd custom (at 1 2.816 ${180 + p.rot}) (size 1.2 0.5) (layers "B.Cu" "B.Mask") ${p.BAT_N.str}
-                (clearance 0.1) (zone_connect 0)
-                (options (clearance outline) (anchor rect))
-                (primitives
-                (gr_poly
-                    (pts
-                    (xy 0.6 0)
-                    (xy -0.6 0)
-                    (xy -0.6 1)
-                    (xy 0 0.4)
-                    (xy 0.6 1)
-                    )
-                    (width 0))
-                ) )
-            (pad 2 smd custom (at -1 2.816 ${180 + p.rot}) (size 1.2 0.5) (layers "B.Cu" "B.Mask") ${p.BAT_P.str}
-                (clearance 0.1) (zone_connect 0)
-                (options (clearance outline) (anchor rect))
-                (primitives
-                (gr_poly
-                    (pts
-                    (xy 0.6 0)
-                    (xy -0.6 0)
-                    (xy -0.6 1)
-                    (xy 0 0.4)
-                    (xy 0.6 1)
-                    )
-                    (width 0))
-                ) )
-            (pad 2 smd custom (at 1 2.816 ${180 + p.rot}) (size 1.2 0.5) (layers "F.Cu" "F.Mask") ${p.BAT_P.str}
-                (clearance 0.1) (zone_connect 0)
-                (options (clearance outline) (anchor rect))
-                (primitives
-                (gr_poly
-                    (pts
-                    (xy 0.6 0)
-                    (xy -0.6 0)
-                    (xy -0.6 1)
-                    (xy 0 0.4)
-                    (xy 0.6 1)
-                    )
-                    (width 0))
-                ) )
-                `
+                    ${local_nets[1]})
+                (pad 1 smd custom (at -1 2.816 ${180 + p.rot}) (size 1.2 0.5) (layers "F.Cu" "F.Mask") ${p.BAT_N.str}
+                    (clearance 0.1) (zone_connect 0)
+                    (options (clearance outline) (anchor rect))
+                    (primitives
+                    (gr_poly
+                        (pts
+                        (xy 0.6 0)
+                        (xy -0.6 0)
+                        (xy -0.6 1)
+                        (xy 0 0.4)
+                        (xy 0.6 1)
+                        )
+                        (width 0))
+                    ) )
+                (pad 1 smd custom (at 1 2.816 ${180 + p.rot}) (size 1.2 0.5) (layers "B.Cu" "B.Mask") ${p.BAT_N.str}
+                    (clearance 0.1) (zone_connect 0)
+                    (options (clearance outline) (anchor rect))
+                    (primitives
+                    (gr_poly
+                        (pts
+                        (xy 0.6 0)
+                        (xy -0.6 0)
+                        (xy -0.6 1)
+                        (xy 0 0.4)
+                        (xy 0.6 1)
+                        )
+                        (width 0))
+                    ) )
+                (pad 2 smd custom (at -1 2.816 ${180 + p.rot}) (size 1.2 0.5) (layers "B.Cu" "B.Mask") ${p.BAT_P.str}
+                    (clearance 0.1) (zone_connect 0)
+                    (options (clearance outline) (anchor rect))
+                    (primitives
+                    (gr_poly
+                        (pts
+                        (xy 0.6 0)
+                        (xy -0.6 0)
+                        (xy -0.6 1)
+                        (xy 0 0.4)
+                        (xy 0.6 1)
+                        )
+                        (width 0))
+                    ) )
+                (pad 2 smd custom (at 1 2.816 ${180 + p.rot}) (size 1.2 0.5) (layers "F.Cu" "F.Mask") ${p.BAT_P.str}
+                    (clearance 0.1) (zone_connect 0)
+                    (options (clearance outline) (anchor rect))
+                    (primitives
+                    (gr_poly
+                        (pts
+                        (xy 0.6 0)
+                        (xy -0.6 0)
+                        (xy -0.6 1)
+                        (xy 0 0.4)
+                        (xy 0.6 1)
+                        )
+                        (width 0))
+                    ) )
+            `
         const standard_closing = `
             )
         `
