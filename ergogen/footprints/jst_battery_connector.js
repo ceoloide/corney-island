e7b716445492d486d1420c764b297da3fe56aae5
Marco Massarelli <marco.massarelli@gmail.com>
2024-01-28 15:50:21 +0000

    Add license header and rename parameters

--- ergogen/footprints/jst_battery_connector.js
+++ ergogen/footprints/jst_battery_connector.js
@@ -1,15 +1,58 @@
+// Copyright (c) 2023 Marco Massarelli
+//
+// SPDX-License-Identifier: MIT
+//
+// To view a copy of this license, visit https://opensource.org/license/mit/
+//
+// Author: @ceoloide
+//
+// Description:
+//  A reversible JST PH 2.0mm footprint with support for solder jumpers and traces. This is
+//  the same part sold at Typeractive.xyz and LCSC.
+//
+//  Note that the footprint's courtyard includes the space required for the male connector
+//  and its cables. Make sure to leave enough room in front of the connector. The silkscreen
+//  includes a handy reference for positive and negative terminals that remains visible
+//  after the connector is soldered, to ensure wire polarity is correct.
+//
+// Datasheet:
+//  https://cdn.shopify.com/s/files/1/0618/5674/3655/files/JST-S2B-PH-K.pdf?v=1670451309
+//
+// Params
+//    side: default is F for Front
+//      the side on which to place the single-side footprint and designator, either F or B
+//    reversible: default is false
+//      if true, the footprint will be placed on both sides so that the PCB can be
+//      reversible
+//    include_traces: default is true
+//      if true it will include traces that connect the jumper pads to the connector pins
+//    trace_width: default is 0.250mm
+//      allows to override the trace width that connects the jumper pads to the connector
+//      pins. Not recommended to go below 0.25mm.
+//    silkscreen: default is true
+//      if true it will include the silkscreen. Recommended to be true to ensure connector
+//      polarity is not reversed, which can lead to shorting and damage to the MCU
+//    fabrication: default is true
+//      if true it will include the outline of the connector in the fabrication layer
+//    courtyard: default is true
+//      if true it will include a courtyard outline around the connector and in front of it
+//      to also account for the male connector plug and the wires. Recommended to be true
+//      at least once in the development of a board to confirm sufficient clearance for the
+//      connector and wires.
+
+
 module.exports = {
     params: {
         designator: 'JST',
         side: 'F',
+        reversible: false,
+        include_traces: true,
+        trace_width: 0.250,
+        include_silkscreen: true,
+        include_fabrication: true,
+        include_courtyard: true,
         BAT_P: { type: 'net', value: 'BAT_P' },
         BAT_N: { type: 'net', value: 'GND' },
-        reversible: false,
-        traces: true,
-        trace_width: 0.250,
-        silkscreen: true,
-        fabrication: true,
-        courtyard: true,
     },
     body: p => {
 
@@ -52,7 +95,7 @@ module.exports = {
         ];
 
         const standard_opening = `
-            (module "JST_PH_S2B-PH-K" (layer ${p.side}.Cu) (tedit 6135B927)
+            (module "ceoloide/jst_battery_connector" (layer ${p.side}.Cu) (tedit 6135B927)
                 ${p.at /* parametric position */}
 
                 (descr "JST PH series connector, S2B-PH-K (http://www.jst-mfg.com/product/pdf/eng/ePH.pdf)")
@@ -260,24 +303,24 @@ module.exports = {
         let final = standard_opening;
 
         if(p.side == "F" || p.reversible) {
-            if(p.fabrication) {
+            if(p.include_fabrication) {
                 final += front_fabrication;
             }
-            if(p.courtyard) {
+            if(p.include_courtyard) {
                 final += front_courtyard;
             }
-            if(p.silkscreen) {
+            if(p.include_silkscreen) {
                 final += front_silkscreen;
             }
         }
         if(p.side == "B" || p.reversible) {
-            if(p.fabrication) {
+            if(p.include_fabrication) {
                 final += back_fabrication;
             }
-            if(p.courtyard) {
+            if(p.include_courtyard) {
                 final += back_courtyard;
             }
-            if(p.silkscreen) {
+            if(p.include_silkscreen) {
                 final += back_silkscreen;
             }
         }
@@ -289,7 +332,7 @@ module.exports = {
             final += back_pads;
         }
         final += standard_closing;
-        if(p.reversible && p.traces) {
+        if(p.reversible && p.include_traces) {
             final += reversible_traces;
         } 
         return final;
